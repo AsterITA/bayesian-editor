131654059e42072c6d80669f46f3279a275327dd
AsterITA <fabrizio.musella@gmail.com>
2019-11-22 19:44:38 +0100

    Fixed inference window, there were too many columns added

--- NodeCPTGui.py
+++ NodeCPTGui.py
@@ -23,7 +23,6 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         self.font.setWeight(75)
         self.grid = QtWidgets.QGridLayout()
         emptyBox = QtWidgets.QLabel()  # Box vuoto in alto a sinistra nella griglia
-        self.grid.addWidget(emptyBox, 0, 0, 1, self.numParents)
         # Metto il nome del nodo attuale in alto a destra nella griglia
         label = QtWidgets.QLabel()
         label.setFont(self.font)
@@ -32,6 +31,7 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         var = -1
         if self.posterior is None:
             if self.numParents > 0:
+                self.grid.addWidget(emptyBox, 0, 0, 1, self.numParents)
                 self.grid.addWidget(label, 0, self.numParents, 1, 3)
                 # Creo la la riga delle variabili
                 for var in range(self.numParents):
@@ -46,6 +46,7 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
             else:
                 self.grid.addWidget(label, 0, 0, 1, 3)
         else:
+            self.grid.addWidget(emptyBox, 0, 0, 1, 2)
             self.grid.addWidget(label, 0, 0, 1, 2)
         # Creo la riga del vero/falso affianco le variabili
         falseLabel = QtWidgets.QLabel()
