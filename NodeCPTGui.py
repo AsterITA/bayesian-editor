daa5f367d61ad0066f8c2c8cbd398f1fdd9a3399
AsterITA <fabrizio.musella@gmail.com>
2020-01-14 11:57:29 +0100

    Code refactoring

--- NodeCPTGui.py
+++ NodeCPTGui.py
@@ -1,3 +1,4 @@
+import pyqtgraph as pg
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QScrollArea
 
@@ -11,117 +12,127 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         self.numParents = self.nodeCPT.nbrDim() - 1  # Numero di genitori
         self.nodeName = nodeName  # Nome del nodo
         self.scrollArea = QScrollArea()
+        self.setWindowTitle("CPT of " + self.nodeName)
         if self.nodeCPT.var_dims[-1] == 2:  # If it's a LabelizedVariable
+            self.grid = QtWidgets.QGridLayout()
+            central_widget = QtWidgets.QWidget()
+            central_widget.setLayout(self.grid)
+            self.scrollArea.setWidgetResizable(True)
+            self.scrollArea.setWidget(central_widget)
+            self.setCentralWidget(self.scrollArea)
             self.labelizedVariable = True
             self.sliders = []
             self.trueLabels = []
             self.falseLabels = []
+            self.setupLabelizedUI()
         else:  # If it's a RangeVariable
             self.labelizedVariable = False
+            self.graphWidget = pg.PlotWidget()
+            self.verticalLayout = QtWidgets.QVBoxLayout()
+            self.verticalLayout.setObjectName("verticalLayout_2")
+            self.setCentralWidget(self.graphWidget)
+            hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+
+            # plot data: x, y values
+            self.graphWidget.plot(hour, temperature)
             self.boxes = []
-        self.setupUI()
+            self.setupRangeUI()
 
-    def setupUI(self):
-        self.setWindowTitle("CPT of " + self.nodeName)
+    def setupRangeUI(self):
+        print("ciao")
 
+    def setupLabelizedUI(self):
         self.font = QtGui.QFont()
         self.font.setPointSize(12)
         self.font.setBold(True)
         self.font.setWeight(75)
-        self.grid = QtWidgets.QGridLayout()
         # Metto il nome del nodo attuale in alto a destra nella griglia
         label = QtWidgets.QLabel()
         label.setFont(self.font)
         label.setAlignment(QtCore.Qt.AlignCenter)
         label.setText(self.nodeName)
-        var = -1
-        if self.labelizedVariable:
-            if self.posterior is None:
-                if self.numParents > 0:
-                    emptyBox = QtWidgets.QLabel()  # Box vuoto in alto a sinistra nella griglia
-                    self.grid.addWidget(emptyBox, 0, 0, 1, self.numParents)
-                    self.grid.addWidget(label, 0, self.numParents, 1, 3)
-                    # Creo la la riga delle variabili
-                    for var in range(self.numParents):
-                        label = QtWidgets.QLabel()
-                        label.setFont(self.font)
-                        label.setAlignment(QtCore.Qt.AlignCenter)
-                        label.setText(self.nodeCPT.var_names[var])
-                        self.grid.addWidget(label, 1, var, 1, 1)
-
-                    # Creo la griglia dei vero/falso delle rispettive variabili sulla sinistra
-                    domainSet = []
-                    for parent in self.nodeCPT.var_names:
-                        if parent != self.nodeName:
-                            parent = self.scene.bn.variableFromName(parent)
-                            domain = parent.domain()
-                            if domain[0] == '<':  # Nel caso si tratti di una labelizedVariable,
-                                # di solito ci sono solo le label di vero e falso
-                                domain = ["F", "T"]
-                            else:  # Nel caso di una rangeVariable, inserisco nel domainSet tutte le label del dominio
-                                domain = domain[1:-1].split(",")
-                                domain = list(range(int(domain[0]), int(domain[1]) + 1))
-                            domainSet.append(domain)
-                    self.genGrid(1, 0, domainSet)
-                else:
-                    self.grid.addWidget(label, 0, 0, 1, 3)
-            else:
-                self.grid.addWidget(label, 0, 0, 1, 2)
-            # Creo la riga del vero/falso affianco le variabili
-            for col_offset, domain in enumerate(["F", "T"], start=1):
-                label = QtWidgets.QLabel()
-                label.setFont(self.font)
-                label.setAlignment(QtCore.Qt.AlignCenter)
-                label.setText(str(domain))
-                if col_offset == 2:
-                    col_offset += 1  # Offset one more because of the sliders
-                self.grid.addWidget(label, 1, var + col_offset, 1, 1)
-        else:
-            self.grid.addWidget(label, 0, 0, 1, 2)
         if self.posterior is None:
-            # Creo i box delle proabilità
-            if self.labelizedVariable:
-                self.genSliders()
-                # Aggiungo il bottone per confermare il CPT e chiudere la finestra
-                button = QtWidgets.QPushButton('OK', self)
-                self.grid.addWidget(button, self.grid.rowCount(), 0, 1, self.grid.columnCount())
-                button.clicked.connect(self.closeWindow)
+            if self.numParents > 0:
+                emptyBox = QtWidgets.QLabel()  # Box vuoto in alto a sinistra nella griglia
+                self.grid.addWidget(emptyBox, 0, 0, 1, self.numParents)
+                self.grid.addWidget(label, 0, self.numParents, 1, 3)
+                # Creo la la riga delle variabili
+                for var in range(self.numParents):
+                    label = QtWidgets.QLabel()
+                    label.setFont(self.font)
+                    label.setAlignment(QtCore.Qt.AlignCenter)
+                    label.setText(self.nodeCPT.var_names[var])
+                    self.grid.addWidget(label, 1, var, 1, 1)
+
+                # Creo la griglia dei vero/falso delle rispettive variabili sulla sinistra
+                domainSet = []
+                for parent in self.nodeCPT.var_names:
+                    if parent != self.nodeName:
+                        parent = self.scene.bn.variableFromName(parent)
+                        domain = parent.domain()
+                        if domain[0] == '<':  # Nel caso si tratti di una labelizedVariable,
+                            # di solito ci sono solo le label di vero e falso
+                            domain = ["F", "T"]
+                        else:  # Nel caso di una rangeVariable, inserisco nel domainSet tutte le label del dominio
+                            domain = domain[1:-1].split(",")
+                            domain = list(range(int(domain[0]), int(domain[1]) + 1))
+                        domainSet.append(domain)
+                self.genGrid(1, 0, domainSet)
+            else:
+                self.grid.addWidget(label, 0, 0, 1, 3)
+                # Creo i box delle proabilità
+            self.genSliders()
+            # Aggiungo il bottone per confermare il CPT e chiudere la finestra
+            button = QtWidgets.QPushButton('OK', self)
+            self.grid.addWidget(button, self.grid.rowCount(), 0, 1, self.grid.columnCount())
+            button.clicked.connect(self.closeWindow)
 
             # Aggiungo comandi per l'inferenza
             self.verticalLayout = QtWidgets.QVBoxLayout()
             self.verticalLayout.setObjectName("verticalLayout_2")
-            self.inference_header = QtWidgets.QLabel()
-            self.inference_header.setFont(self.font)
-            self.inference_header.setAlignment(QtCore.Qt.AlignBottom | QtCore.Qt.AlignHCenter)
-            self.inference_header.setObjectName("inference_header")
-            self.verticalLayout.addWidget(self.inference_header)
-            self.inferenceMode = QtWidgets.QComboBox()
-            self.inferenceMode.addItems(["Lazy Propagation", "Shafer Shenoy", "Variable Elimination"])
-            self.verticalLayout.addWidget(self.inferenceMode)
-            self.inference_header.setText("-Inference-")
-            self.inference_btn = QtWidgets.QPushButton()
-            self.inference_btn.setObjectName("inference_CPT_btn")
-            self.inference_btn.setText("MAKE INFERENCE")
-            self.inference_btn.clicked.connect(
-                lambda: self.scene.makeInference(self.nodeName, self.inferenceMode.currentIndex()))
-            self.verticalLayout.addWidget(self.inference_btn)
+            self.setupInferenceLayout()
             self.grid.addLayout(self.verticalLayout, self.grid.rowCount(), 0, 1, self.grid.columnCount())
-        elif self.labelizedVariable:
+        else:
+            self.grid.addWidget(label, 0, 0, 1, 2)
             posterior = self.posterior.__str__().split('/')
             falsePosterior = QtWidgets.QLabel()
             falsePosterior.setAlignment(QtCore.Qt.AlignCenter)
             falsePosterior.setText(posterior[0].split('::')[1])
-            self.grid.addWidget(falsePosterior, 2, var + 1, 1, 1)
+            self.grid.addWidget(falsePosterior, 2, 0, 1, 1)
             truePosterior = QtWidgets.QLabel()
             truePosterior.setAlignment(QtCore.Qt.AlignCenter)
             truePosterior.setText(posterior[1].split('::')[1])
-            self.grid.addWidget(truePosterior, 2, var + 2, 1, 1)
+            self.grid.addWidget(truePosterior, 2, 1, 1, 1)
+        # Creo la riga del vero/falso affianco le variabili
+        for col_offset, domain in enumerate(["F", "T"]):
+            label = QtWidgets.QLabel()
+            label.setFont(self.font)
+            label.setAlignment(QtCore.Qt.AlignCenter)
+            label.setText(str(domain))
+            if col_offset == 1 and self.posterior is None:
+                col_offset += 1  # Offset one more because of the sliders
+            if self.posterior is None:
+                self.grid.addWidget(label, 1, self.numParents + col_offset, 1, 1)
+            else:
+                self.grid.addWidget(label, 1, col_offset, 1, 1)
 
-        central_widget = QtWidgets.QWidget()
-        central_widget.setLayout(self.grid)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setWidget(central_widget)
-        self.setCentralWidget(self.scrollArea)
+    def setupInferenceLayout(self):
+        self.inference_header = QtWidgets.QLabel()
+        self.inference_header.setFont(self.font)
+        self.inference_header.setAlignment(QtCore.Qt.AlignBottom | QtCore.Qt.AlignHCenter)
+        self.inference_header.setObjectName("inference_header")
+        self.verticalLayout.addWidget(self.inference_header)
+        self.inferenceMode = QtWidgets.QComboBox()
+        self.inferenceMode.addItems(["Lazy Propagation", "Shafer Shenoy", "Variable Elimination"])
+        self.verticalLayout.addWidget(self.inferenceMode)
+        self.inference_header.setText("-Inference-")
+        self.inference_btn = QtWidgets.QPushButton()
+        self.inference_btn.setObjectName("inference_CPT_btn")
+        self.inference_btn.setText("MAKE INFERENCE")
+        self.inference_btn.clicked.connect(
+            lambda: self.scene.makeInference(self.nodeName, self.inferenceMode.currentIndex()))
+        self.verticalLayout.addWidget(self.inference_btn)
 
     # def genBoxes(self):
     #     listCPT = self.nodeCPT.toarray()
@@ -150,8 +161,8 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         splittedCPT = self.nodeCPT.__str__().split('/')
         col = self.grid.columnCount()
         n_rows = self.grid.rowCount() - 2
-        if n_rows == 0:
-            n_rows = n_rows + 1
+        if n_rows <= 0:
+            n_rows = 1
         cpt_counter = 0
         for row in range(n_rows):
             self.sliders.append(QtWidgets.QSlider(QtCore.Qt.Horizontal))
