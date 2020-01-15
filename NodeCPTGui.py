ee0d2d3cadcac88fc315771d3ecf1101cc1e1a11
AsterITA <fabrizio.musella@gmail.com>
2020-01-15 17:11:23 +0100

    Completed RangeVariable GUI, updateCPT and plot of the CPT

--- NodeCPTGui.py
+++ NodeCPTGui.py
@@ -1,20 +1,36 @@
-import pyqtgraph as pg
+from dataclasses import dataclass
+from itertools import islice
+
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QScrollArea
+from iteration_utilities import deepflatten
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from pylab import *
+from scipy.stats import maxwell, norm
+
+
+@dataclass
+class HistogramArguments:
+    function = None
+    vmin = None
+    vmax = None
 
 
 class Ui_CPTWindow(QtWidgets.QMainWindow):
     def __init__(self, graph_scene, nodeName, posterior=None):
         super().__init__()
+        self.font = QtGui.QFont()
+        self.font.setPointSize(12)
+        self.font.setBold(True)
+        self.font.setWeight(75)
         self.scene = graph_scene
         self.posterior = posterior
         self.nodeCPT = self.scene.bn.cpt(nodeName)  # CPT del nodo
         self.numParents = self.nodeCPT.nbrDim() - 1  # Numero di genitori
         self.nodeName = nodeName  # Nome del nodo
-        self.scrollArea = QScrollArea()
+        self.scrollArea = QtWidgets.QScrollArea()
+        self.grid = QtWidgets.QGridLayout()
         self.setWindowTitle("CPT of " + self.nodeName)
         if self.nodeCPT.var_dims[-1] == 2:  # If it's a LabelizedVariable
-            self.grid = QtWidgets.QGridLayout()
             central_widget = QtWidgets.QWidget()
             central_widget.setLayout(self.grid)
             self.scrollArea.setWidgetResizable(True)
@@ -27,26 +43,115 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
             self.setupLabelizedUI()
         else:  # If it's a RangeVariable
             self.labelizedVariable = False
-            self.graphWidget = pg.PlotWidget()
+            # a figure instance to plot on
+            self.figure = plt.figure()
+            # this is the Canvas Widget that displays the `figure`
+            # it takes the `figure` instance as a parameter to __init__
+            self.canvas = FigureCanvas(self.figure)
+
+            # this is the Navigation widget
+            # it takes the Canvas widget and a parent
+            # self.toolbar = NavigationToolbar(self.canvas, self)
             self.verticalLayout = QtWidgets.QVBoxLayout()
             self.verticalLayout.setObjectName("verticalLayout_2")
-            self.setCentralWidget(self.graphWidget)
-            hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-            temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
-
-            # plot data: x, y values
-            self.graphWidget.plot(hour, temperature)
-            self.boxes = []
+            widget = QtWidgets.QWidget()
+            widget.setLayout(self.verticalLayout)
+            self.setCentralWidget(widget)
+            self.HistogramArguments = []
             self.setupRangeUI()
 
     def setupRangeUI(self):
-        print("ciao")
+        # Metto il nome del nodo attuale in alto a destra nella griglia
+        label = QtWidgets.QLabel()
+        label.setFont(self.font)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setText(self.nodeName)
+        self.verticalLayout.addWidget(label)
+        self.genVariables()
+        label = QtWidgets.QLabel()
+        label.setFont(self.font)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setText("Function")
+        self.grid.addWidget(label, 1, self.numParents)
+        label = QtWidgets.QLabel()
+        label.setFont(self.font)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setText("Start")
+        self.grid.addWidget(label, 1, self.numParents + 1)
+        label = QtWidgets.QLabel()
+        label.setFont(self.font)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setText("Stop")
+        self.grid.addWidget(label, 1, self.numParents + 2)
+        nrows = self.grid.rowCount()
+        if self.numParents == 0:
+            nrows += 1
+        for index, row in enumerate(range(2, nrows)):
+            self.HistogramArguments.append(HistogramArguments())
+            self.HistogramArguments[index].function = QtWidgets.QComboBox()
+            self.HistogramArguments[index].function.addItems(["Norm", "Maxwell"])
+            self.HistogramArguments[index].vmin = QtWidgets.QDoubleSpinBox()
+            self.HistogramArguments[index].vmin.setMinimum(-100)
+            self.HistogramArguments[index].vmin.setMaximum(100)
+            self.HistogramArguments[index].vmax = QtWidgets.QDoubleSpinBox()
+            self.HistogramArguments[index].vmax.setMinimum(-100)
+            self.HistogramArguments[index].vmax.setMaximum(100)
+            self.grid.addWidget(self.HistogramArguments[index].function, row, self.numParents)
+            self.grid.addWidget(self.HistogramArguments[index].vmin, row, self.numParents + 1)
+            self.grid.addWidget(self.HistogramArguments[index].vmax, row, self.numParents + 2)
+
+        widget = QtWidgets.QWidget()
+        widget.setLayout(self.grid)
+        self.verticalLayout.addWidget(widget)
+        # Aggiungo il bottone per confermare il CPT e chiudere la finestra
+        horizontalLayout = QtWidgets.QHBoxLayout()
+        button = QtWidgets.QPushButton('OK', self)
+        button.clicked.connect(self.closeWindow)
+        horizontalLayout.addWidget(button)
+        button = QtWidgets.QPushButton('Apply', self)
+        button.clicked.connect(self.applyCPT)
+        horizontalLayout.addWidget(button)
+        widget = QtWidgets.QWidget()
+        widget.setLayout(horizontalLayout)
+        self.verticalLayout.addWidget(widget)
+        # self.verticalLayout.addWidget(self.toolbar)
+        self.verticalLayout.addWidget(self.canvas)
+        self.setupInferenceLayout()
+        self.plot()
+
+    def plot(self):
+        self.figure.clear()
+        # create an axis
+        ax = self.figure.add_subplot(111)
+        # get the data from the CPT
+        values = []
+        for i in self.nodeCPT.loopIn():
+            values.append(self.nodeCPT.get(i))
+        # plot the data
+        for line in self.make_chunks(values, self.nodeCPT.var_dims[-1]):
+            ax.plot(line)
+        title("P(" + self.nodeName + ")")
+        legendStr = ""
+        for row in range(2, self.grid.rowCount()):
+            if row != 2:
+                legendStr += ";"
+            legendStr += "P(" + self.nodeName + "|"
+            for col, variable in enumerate(self.nodeCPT.var_names):
+                if variable != self.nodeName:
+                    if col != 0:
+                        legendStr += ", "
+                    legendStr += variable + "=" + self.grid.itemAtPosition(row, col).widget().text()
+            legendStr += ")"
+        legend(legendStr.split(";"), loc="best")
+        # refresh canvas
+        self.canvas.draw()
+
+    def make_chunks(self, data, SIZE):
+        it = iter(data)
+        for i in range(0, len(data), SIZE):
+            yield [k for k in islice(it, SIZE)]
 
     def setupLabelizedUI(self):
-        self.font = QtGui.QFont()
-        self.font.setPointSize(12)
-        self.font.setBold(True)
-        self.font.setWeight(75)
         # Metto il nome del nodo attuale in alto a destra nella griglia
         label = QtWidgets.QLabel()
         label.setFont(self.font)
@@ -57,28 +162,7 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
                 emptyBox = QtWidgets.QLabel()  # Box vuoto in alto a sinistra nella griglia
                 self.grid.addWidget(emptyBox, 0, 0, 1, self.numParents)
                 self.grid.addWidget(label, 0, self.numParents, 1, 3)
-                # Creo la la riga delle variabili
-                for var in range(self.numParents):
-                    label = QtWidgets.QLabel()
-                    label.setFont(self.font)
-                    label.setAlignment(QtCore.Qt.AlignCenter)
-                    label.setText(self.nodeCPT.var_names[var])
-                    self.grid.addWidget(label, 1, var, 1, 1)
-
-                # Creo la griglia dei vero/falso delle rispettive variabili sulla sinistra
-                domainSet = []
-                for parent in self.nodeCPT.var_names:
-                    if parent != self.nodeName:
-                        parent = self.scene.bn.variableFromName(parent)
-                        domain = parent.domain()
-                        if domain[0] == '<':  # Nel caso si tratti di una labelizedVariable,
-                            # di solito ci sono solo le label di vero e falso
-                            domain = ["F", "T"]
-                        else:  # Nel caso di una rangeVariable, inserisco nel domainSet tutte le label del dominio
-                            domain = domain[1:-1].split(",")
-                            domain = list(range(int(domain[0]), int(domain[1]) + 1))
-                        domainSet.append(domain)
-                self.genGrid(1, 0, domainSet)
+                self.genVariables()
             else:
                 self.grid.addWidget(label, 0, 0, 1, 3)
                 # Creo i box delle proabilità
@@ -117,6 +201,30 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
             else:
                 self.grid.addWidget(label, 1, col_offset, 1, 1)
 
+    def genVariables(self):
+        # Creo la la riga delle variabili
+        for var in range(self.numParents):
+            label = QtWidgets.QLabel()
+            label.setFont(self.font)
+            label.setAlignment(QtCore.Qt.AlignCenter)
+            label.setText(self.nodeCPT.var_names[var])
+            self.grid.addWidget(label, 1, var, 1, 1)
+
+        # Creo la griglia dei vero/falso delle rispettive variabili sulla sinistra
+        domainSet = []
+        for parent in self.nodeCPT.var_names:
+            if parent != self.nodeName:
+                parent = self.scene.bn.variableFromName(parent)
+                domain = parent.domain()
+                if domain[0] == '<':  # Nel caso si tratti di una labelizedVariable,
+                    # di solito ci sono solo le label di vero e falso
+                    domain = ["F", "T"]
+                else:  # Nel caso di una rangeVariable, inserisco nel domainSet tutte le label del dominio
+                    domain = domain[1:-1].split(",")
+                    domain = list(range(int(domain[0]), int(domain[1]) + 1))
+                domainSet.append(domain)
+        self.genGrid(1, 0, domainSet)
+
     def setupInferenceLayout(self):
         self.inference_header = QtWidgets.QLabel()
         self.inference_header.setFont(self.font)
@@ -133,29 +241,6 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         self.inference_btn.clicked.connect(
             lambda: self.scene.makeInference(self.nodeName, self.inferenceMode.currentIndex()))
         self.verticalLayout.addWidget(self.inference_btn)
-
-    # def genBoxes(self):
-    #     listCPT = self.nodeCPT.toarray()
-    #     col = len(self.nodeCPT.var_names)
-    #     n_rows = self.grid.rowCount() - 2
-    #     defaultValue = 1 / self.nodeCPT.var_dims[-1]
-    #     if n_rows == 0:
-    #         n_rows += 1
-    #     for row in range(n_rows):
-    #         value = 0
-    #         for i in range(self.nodeCPT.var_dims[-1]):
-    #             value += listCPT[row][
-    #                 i]  # Check if the sum of the values in the CPT is 1, otherwise it was not setted and the default value wil be setted
-    #         for i in range(self.nodeCPT.var_dims[-1]):
-    #             self.boxes[row][i].append(QtWidgets.QDoubleSpinBox())
-    #             self.boxes[row][i].setMaximum(1)
-    #             self.boxes[row][i].setMinimum(0)
-    #             self.boxes[row][i].setSingleStep(0.01)
-    #             if value != 1:  # Set the default value
-    #                 self.boxes[row][i].setValue(defaultValue)
-    #             else:  # Set the read value
-    #                 self.boxes[row][i].setValue(listCPT[row][i])
-    #             self.grid.addWidget(self.boxes[row][i], row + 2, col + i)
 
     def genSliders(self):
         splittedCPT = self.nodeCPT.__str__().split('/')
@@ -197,13 +282,36 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
         falseLabel.setText(str(round(1 - value, 2)))  # Approssimo per evitare rappresentazioni imprecise del float
         trueLabel.setText(str(value))
 
-    def closeWindow(self):
+    def updateCPT(self):
         probabilities = []
-        for false, true in zip(self.falseLabels, self.trueLabels):
-            probabilities.append(float(false.text()))
-            probabilities.append(float(true.text()))
-        self.nodeCPT.fillWith(probabilities)
+        if self.labelizedVariable:
+            for false, true in zip(self.falseLabels, self.trueLabels):
+                probabilities.append(float(false.text()))
+                probabilities.append(float(true.text()))
+            self.nodeCPT.fillWith(probabilities)
+        else:
+            # Calculate all the values
+            functions = []
+            for args in self.HistogramArguments:
+                function = args.function.currentIndex()
+                if function == 0:
+                    function = norm()
+                else:
+                    function = maxwell()
+                functions.append(self.normalize(function, args.vmin.value(), args.vmax.value()))
+            # Flatten all the values in a single list
+            functions = list(deepflatten(functions))
+            # Update the cpt with the values
+            for cpt, value in zip(self.nodeCPT.loopIn(), functions):
+                self.nodeCPT.set(cpt, value)
+
+    def closeWindow(self):
+        self.updateCPT()
         self.close()
+
+    def applyCPT(self):
+        self.updateCPT()
+        self.plot()
 
     def genGrid(self, firstRow, col, domains):
         if col >= len(domains):
@@ -223,3 +331,8 @@ class Ui_CPTWindow(QtWidgets.QMainWindow):
                 self.grid.addWidget(label, firstRow + row, col, 1, 1)
             for i in range(numDomains):
                 self.genGrid(firstRow + i * step, col + 1, domains)
+
+    # we truncate a pdf, so we need to normalize
+    def normalize(self, rv, vmin, vmax):
+        pdf = rv.pdf(linspace(vmin, vmax, self.nodeCPT.var_dims[-1]))
+        return pdf / sum(pdf)
